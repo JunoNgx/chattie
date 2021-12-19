9691149fb104bad006aa5930edf7bf60409ba919
Juno Nguyen <juno.ngx@gmail.com>
2021-12-19 17:11:17 +0800

    Fix indentations in router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,29 +4,29 @@ import About from '../views/About.vue'
 import Settings from '../views/Settings.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    component: About
-  }, {
-    path: '/settings',
-    name: 'Settings',
-    component: Settings
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // // route level code-splitting
+        // // this generates a separate chunk (about.[hash].js) for this route
+        // // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        component: About
+    }, {
+        path: '/settings',
+        name: 'Settings',
+        component: Settings
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
