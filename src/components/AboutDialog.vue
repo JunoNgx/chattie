505a965c34b184491591ebda2c91d9444c464b23
Juno Nguyen <juno.ngx@gmail.com>
2021-12-19 14:45:09 +0800

    Draft about dialog content

---
+++ src/components/AboutDialog.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import ModalBase from './ModalBase.vue'
+
+    export default defineComponent ({
+        name: "AboutDialog",
+        props: {
+            close: Function
+        },
+        components: {
+            ModalBase
+        }
+    })
+</script>
+
+<template>
+    <modal-base :close="close">
+        <p>Chattie is a web application made by Juno Nguyen.</p>
+    </modal-base>
+</template>
+
+<style lang="sass" scope>
+
+</style>
